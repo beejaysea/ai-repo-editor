9bd8a8e334ddfbb45a2ecb3021f15522a53a23c0
bjc <beejaysea@users.noreply.github.com>
2024-10-25 18:29:26 -0700

    baseline

--- anthropic_editor.py
+++ anthropic_editor.py
@@ -1,36 +1,53 @@
 import anthropic
 from dotenv import load_dotenv
+from anthropic.types.beta import BetaTextBlock, BetaToolUseBlock
 
 load_dotenv()
 
 client = anthropic.Anthropic()
 
-message="""Generate a well-structured architecture for a new software project.
+input_goal="""Generate a well-structured architecture for a new software project.
 The project is a web application that allows users to create and share documents.
 The application should have a user-friendly interface that allows users to easily create, edit, and share documents.
 The application should also have a robust backend that can handle a large number of users and documents.
 """
-response = client.beta.messages.create(
-    model="claude-3-5-sonnet-20241022",
-    max_tokens=1024,
-    tools=[
-        # {
-        #   "type": "computer_20241022",
-        #   "name": "computer",
-        #   "display_width_px": 1024,
-        #   "display_height_px": 768,
-        #   "display_number": 1,
-        # },
-        {
-          "type": "text_editor_20241022",
-          "name": "str_replace_editor"
-        },
-        # {
-        #   "type": "bash_20241022",
-        #   "name": "bash"
-        # }
-    ],
-    messages=[{"role": "user", "content": message}],
-    betas=["computer-use-2024-10-22"],
-)
-print(response)
+
+done = False
+while not done:
+    response = client.beta.messages.create(
+        model="claude-3-5-sonnet-20241022",
+        max_tokens=1024,
+        tools=[
+            # {
+            #   "type": "computer_20241022",
+            #   "name": "computer",
+            #   "display_width_px": 1024,
+            #   "display_height_px": 768,
+            #   "display_number": 1,
+            # },
+            {
+            "type": "text_editor_20241022",
+            "name": "str_replace_editor"
+            },
+            # {
+            #   "type": "bash_20241022",
+            #   "name": "bash"
+            # }
+        ],
+        messages=[{"role": "user", "content": input_goal}],
+        betas=["computer-use-2024-10-22"],
+    )
+    if response.stop_reason=='tool_use':
+        messages = response.content
+        for message in messages:
+            if isinstance(message, BetaTextBlock):
+                output = message.text
+                print(output)
+            if isinstance(message, BetaToolUseBlock):
+                tool = message.input['command']
+                print(tool)
+
+    if response.stop_reason in ['end_turn', 'max_tokens', 'stop_sequence']:
+        print("Stopped: ", response.stop_reason)
+        print(response.content[0].text)
+        done = True
